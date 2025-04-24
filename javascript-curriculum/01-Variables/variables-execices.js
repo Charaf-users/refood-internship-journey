e17789d851f9699e0594b5ea45b5e3408229928f
CHARAF EDDIN JADOR <eaglescharaf50@gmail.com>
2025-04-24 13:27:04 +0100

    exercices completed

--- javascript-curriculum/01-Variables/variables-execices.js
+++ javascript-curriculum/01-Variables/variables-execices.js
@@ -1,26 +1,32 @@
-var companyName ="Refood";
+ // Task 1: Declare Variables
+
+ //declare variables using var
+ var companyName ="Refood";
+  //declare variables using let
 let InterName = "John Doe";
+   //declare variables using const
 const intershipYear = 2025;
 
+ 
+// print value 
+ console.log(companyName); // Refood
+ console.log(InterName);// john Doe
+ console.log(intershipYear);//2025 
+
+ //Task 3 : ressign values
+// Reassign the values
  companyName ="Refood Tech";
  InterName = "Jim smith"
 
- console.log(companyName);
- console.log(InterName);
- console.log(intershipYear); 
 
-
-
-
-
-
+// Task 4: Use Different Data Types 
   let age = 22;
   let isActive = true ;
   let skills = ["JS","HTML"];
   let details = {departement : "Tech", Level : "Intern"};
  console.log(age,isActive,skills,details);
 
-
+// Task 5: typeof Check 
   console.log(typeof companyName );
   console.log(typeof age );
   console.log(typeof isActive);
